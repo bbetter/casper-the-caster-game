fc1faf51be0b5aff7a09a38bfb0a85600c3dac77
Andrii <andriy.ihorowych@gmail.com>
2023-03-26 13:51:08 +0300

    [WIP] health bar

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -4,18 +4,19 @@ extends CanvasLayer
 
 # Labels
 @onready var _node_killed_label = get_node("KilledLabel")
-@onready var _node_hp_label = get_node("HpLabel")
+@onready var _node_hp = get_node("HpRect") as TextureRect
 
 # Properties
 @export var killed_count := 0 : set = _set_killed_count
 @export var hp := 0 : set = _set_hp
 
+func _ready():
+	_node_hp.modulate = Color(1,1,1,1)
+
 func _set_killed_count(new_value):
 	killed_count = new_value
 	if(_node_killed_label != null):
-		_node_killed_label.text = "KILLED: " + str(new_value)
+		_node_killed_label.text = str(new_value)
 	
 func _set_hp(new_value):
 	hp = new_value
-	if(_node_hp_label != null):
-		_node_hp_label.text = "HP: " + str(new_value)
